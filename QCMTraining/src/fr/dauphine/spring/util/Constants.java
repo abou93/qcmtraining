f1b7e7dd9f592953c561a38e0d38ce90cbeeabb0
thierry.khamsy <thierry.khamsy@7d357aea-5ee0-280c-5719-9c183f8b8e2e>
2010-05-20 19:15:23 +0000

    Constants.java test

--- QCMTraining/src/fr/dauphine/spring/util/Constants.java
+++ QCMTraining/src/fr/dauphine/spring/util/Constants.java
@@ -13,5 +13,14 @@ public final class Constants {
 	public static final short PARAM_DEFAULT_NUMBER_REPONSE = 4;
 	public static final short PARAM_DEFAULT_NUMBER_QUESTION = 1;
 	public static final String PARAM_DOMAIN_NAME = "domainName";
+	
+	public static final String PARAM_ISADMIN = "isAdmin";
+	public static final String PARAM_ISLOGGED = "isLogged";
+	public static final String PARAM_PAGE_CONTENT = "pageContent";
+	
+	public static final String PARAM_CONTENT_INDEX = "/jsp/content/index.jsp";
+	public static final String PARAM_CONTENT_INDEXADMIN = "/jsp/content/indexAdmin.jsp";
+	public static final String PARAM_CONTENT_INDEXUSER = "/jsp/content/indexUser.jsp";
+	public static final String PARAM_CONTENT_CREERUSER = "/jsp/content/creerUser.jsp";
 
 }
